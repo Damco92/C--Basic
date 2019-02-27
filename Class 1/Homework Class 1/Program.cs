5ecbd51fc3966ff181d1fb27547821294d5f52b0
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-02-27 13:05:24 +0100

    Homework done

--- Class 1/Homework Class 1/Program.cs
+++ Class 1/Homework Class 1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Homework_Class_1
@@ -10,7 +11,19 @@ namespace Homework_Class_1
     {
         static void Main(string[] args)
         {
-            string myString = "fdssdf";
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("RED LIGHT PLEASE STOP");
+            Thread.Sleep(10000);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("YELLOW LIGHT PREPARE");
+            Thread.Sleep(5000);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("GREEN LIGHT YOU CAN GO");
+            Thread.Sleep(20000);
+            Console.Read();
         }
     }
 }
