d90c61caa8580f4f91dfd24cdcabbf6def94f368
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-02-28 18:55:27 +0100

    new homework class 2 added

---
+++ Class 2/Homework2Exercise -2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double decimalOne;
+            //decimalOne = 2.45;
+            //double decimalTwo;
+            //decimalTwo = 5.66;
+            //double result;
+            //result = decimalTwo / decimalOne;
+            //Console.WriteLine("The result from " + decimalTwo + " devided with " + decimalOne + " equals to " + result +".");
+            //Console.Read();
+            //int numberOne;
+            //int numberTwo;
+            //numberOne = 4;
+            //numberTwo = 2;
+            //int result;
+            //result = numberOne / numberTwo;
+            //Console.WriteLine("The result of the two numbers devided is " + result + ".");
+            //Console.Read();
+            Console.WriteLine("Plesae enter the first number");
+            string firstStrNum = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            string secStrNum = Console.ReadLine();
+            int firstNum;
+            int secNum;
+            bool castNumOne = Int32.TryParse(firstStrNum, out firstNum);
+            bool castNumTwo = Int32.TryParse(secStrNum, out secNum);
+            if (castNumOne == true && castNumTwo == true)
+            {
+                if (firstNum > secNum)
+                {
+                    Console.WriteLine("First Num is bigger");
+                }
+                else
+                {
+                    Console.WriteLine("Second num is bigger");
+                }
+                if (firstNum%2 == 0)
+                {
+                    Console.WriteLine("First num is even");
+                } else
+                {
+                    Console.WriteLine("First num is odd");
+                }
+                if (secNum % 2 == 0)
+                {
+                    Console.WriteLine("Second num is even");
+                } else
+                {
+                    Console.WriteLine("Second num is odd");
+                }
+            } else
+            {
+                Console.WriteLine("Some of the numbers is not convertible");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Class 2/Homework2Exercise-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number from 1 to 3");
+            string firstNum;
+            firstNum = Console.ReadLine();
+            int num;
+            bool castNum = Int32.TryParse(firstNum, out num);
+            if (castNum == true)
+            {
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Number entered one");
+                        break;
+                    case 2:
+                        Console.WriteLine("Number entered two");
+                        break;
+                    case 3:
+                        Console.WriteLine("Number entered three");
+                        break;
+                    default:
+                        Console.WriteLine("Number entered is smaller then one or bigger then three");
+                        break;
+                 } 
+            } else
+            {
+                Console.WriteLine("Input enterd is not a number");
+            }
+            Console.Read();
+            
+        }
+    }
+}
