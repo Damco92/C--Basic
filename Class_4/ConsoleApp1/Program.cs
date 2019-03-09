8b57a0295c7db4a119c09c3efb127a8cb6b539ca
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-03-09 17:06:19 +0100

    Homework 1 exe 1

---
+++ Class_4/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateAge(1989);
+            Console.Read();
+        }
+
+        static void CalculateAge(int birthYear)
+        {
+            Console.WriteLine($"Your birth year is {birthYear}");
+            int currentYear = DateTime.Now.Year;
+            int personsAge = currentYear - birthYear;
+            Console.WriteLine($"The persons age is {personsAge}");
+        }
+    }
+}
