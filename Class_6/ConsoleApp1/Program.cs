60762482854ae59ee1802c8f9ef18fa270b32866
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-03-18 10:32:46 +0100

    last changes to class 6 homeworks

--- Class_6/ConsoleApp1/Program.cs
+++ Class_6/ConsoleApp1/Program.cs
@@ -16,78 +16,92 @@ namespace ConsoleApp1
 
         static void NumberStats()
         {
-            Console.WriteLine("Pleaase enter a number");
-            string numberInput = Console.ReadLine();
-            int num;
-            bool castInt = Int32.TryParse(numberInput, out num);
-            double dec;
-            bool castDouble = Double.TryParse(numberInput, out dec); 
+            Console.WriteLine("Please enter the  number");
             bool valid = true;
-            
+
             while (valid)
             {
-                if (castInt)
-                {
-                    if (num > 0)
-                    {
-                        Console.WriteLine("POSITIVE");
-                    } else if (num<0)
-                    {
-                        Console.WriteLine("NEGATIVE");
-                    }
-                    else 
-                    {
-                        Console.WriteLine("ZERO");
-                    }
-                    Console.WriteLine("INTIGER");
+                checkIfPositive();
+                checkIfIntOrDouble();
+                checkIfOddOrEven();
 
-                    if (num % 2 == 0)
-                    {
-                        Console.WriteLine("EVEN");
-                    } else if (num % 2 == 1)
-                    {
-                        Console.WriteLine("ODD");
-                    }
-                } else if (castDouble)
+                
+                Console.WriteLine("Please enter Y if you want to test an other number or X to exit");
+                string continueExe = Console.ReadLine();
+                if (continueExe == "Y")
                 {
-                    if (dec > 0) Console.WriteLine("Positive");
-                    else if (dec < 0) Console.WriteLine("Negative");
-                    else Console.WriteLine("Zero");
-                    Console.WriteLine("Double");
-                    string decimalNumber = dec.ToString();
-                    string lastDecimal = decimalNumber.Substring(decimalNumber.Length-1);
-                    int finalNum = Convert.ToInt32(lastDecimal);
-                    if (finalNum % 2 == 0) Console.WriteLine("Even decimal");
-                    else if (finalNum % 2 == 1) Console.WriteLine("Odd decimal");
+                    Console.WriteLine("Plesae enter new number");
+                    continue;
                 }
-                else
+                else if (continueExe == "X")
                 {
-                    throw new Exception("Wrong input you have to enter either intiger or double");
+                    Console.WriteLine("Press enter to exit");
+                    break;
+                } else
+                {
+                    Console.WriteLine("You must enter eather Y or N");
+                    continue;
                 }
-                Console.WriteLine("Would you like to try again? Press Y to continue or x for exit");
-                string goFurther = Console.ReadLine();
-                if (goFurther == "Y")
-                {
-                    Console.WriteLine("Please enter next number");
-                    string secondNumber = Console.ReadLine();
-                    int newInt;
-                    double newDouble;
-                    bool castSecondInt = Int32.TryParse(secondNumber, out newInt);
-                    bool castSecDouble = Double.TryParse(secondNumber, out newDouble);
-                    if (castSecondInt)
-                    { 
-                        num = newInt;
-                        continue;
-                    }
-                    else if (castSecDouble)
-                    {
-                        dec = newDouble;
-                        continue;
-                    }
-                } 
-                else if (goFurther == "X") break;
-                else Console.WriteLine("Invalid input"); break;
             }
+            
+        }
+
+        static string checkIfPositive()
+        {
+            string stringNumber = Console.ReadLine();
+            double myDouble;
+            bool cast = double.TryParse(stringNumber, out myDouble);
+            if (cast)
+            {
+                if (myDouble > 0) Console.WriteLine("POSITIVE");
+                else if (myDouble < 0) Console.WriteLine("NEGATIVE");
+                else Console.WriteLine("ZERO");
+            }
+            else
+            {
+                Console.WriteLine("The input was not a number!");
+            }
+            return stringNumber;
+        }
+        static string checkIfIntOrDouble()
+        {
+            checkIfPositive();
+            double myDouble;
+            int myIntiger;
+            bool castInt = Int32.TryParse(checkIfPositive(), out myIntiger);
+            bool cast = double.TryParse(checkIfPositive(), out myDouble);
+            if (cast)
+            {
+                if (myDouble % 1 == 0) Console.WriteLine("INTIGER");
+                else if (myDouble % 1 != 0) Console.WriteLine("DOUBLE");
+            }
+            else if (castInt)
+            {
+                if (myDouble % 1 == 0) Console.WriteLine("INTIGER");
+                else if (myDouble % 1 != 0) Console.WriteLine("DOUBLE");
+            }
+            else
+            {
+                Console.WriteLine("The input was not a number!");
+            }
+            return checkIfPositive();
+        }
+
+        static string checkIfOddOrEven()
+        {
+            checkIfPositive();
+            double myDouble;
+            bool cast = double.TryParse(checkIfPositive(), out myDouble);
+            if (cast)
+            {
+                if (myDouble % 2 == 0) Console.WriteLine("EVEN");
+                else if (myDouble % 2 == 1) Console.WriteLine("ODD");
+            }
+            else
+            {
+                Console.WriteLine("The input was not a number!");
+            }
+            return checkIfPositive();
         }
     }
 }
