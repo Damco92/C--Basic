3b400f7fcff31fb45b2a432c393f71ebcff3ed64
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-03-18 12:31:57 +0100

    changes

---
+++ Class_6/ConsoleApp2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] Users = new User[3];
+
+            User user1 = new User()
+            {
+                Id = 123,
+                Username = "dam-dam"
+            };
+            user1.Messages[0] = $"Hello Damjan nice to meet you";
+            user1.SetPassword("mypass123");
+
+            User user2 = new User()
+            {
+                Id = 345,
+                Username = "kimura"
+            };
+            user2.Messages[0] = "Hello Masahiko  wellcome";
+            user2.SetPassword("Masahikokimura1");
+
+            User user3 = new User()
+            {
+                Id = 567,
+                Username = "Bujinkan"
+            };
+            user3.Messages[0] = "Hello Bujinkan wellcome";
+            user3.SetPassword("MasakiHatsumi");
+
+            Users[0] = user1;
+            Users[1] = user2;
+            Users[2] = user3;
+            Console.WriteLine("Please enter your username");
+            string name = Console.ReadLine();
+            //checkUsers(name, Users);
+
+            Console.Read();
+        }
+
+        //static void checkUsers(string name, User[] Users)
+        //{
+        //    int found = 0;
+        //    foreach (User item in Users) 
+        //    {
+        //        if (item.Username == name )
+        //        {
+        //            found = 1;
+        //            for (int i = 0; i < item.Messages.Length; i++)
+        //            {
+        //                Console.WriteLine($"The message for the user with username {item.Username} is {item.Messages[i]}");
+        //            }
+        //        }
+
+        //        if (found == 0)
+        //        {
+        //            Console.WriteLine("User is not found");
+        //            Console.WriteLine("Do you want to register? y/n");
+        //            //string choise = Console.ReadLine();
+        //            //if (choise == "y")
+        //            //{
+        //                //Register (User[] Users);
+        //            //}
+        //        }
+        //    }
+        //}
+
+
+    }
+}
---
+++ Class_6/ConsoleApp2/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        private string Password { get; set; }
+        public string[] Messages;
+
+        public string SetPassword(string myPass)
+        {
+            if (!string.IsNullOrWhiteSpace(myPass))
+            {
+                Password = myPass;
+                return Password;
+            }
+            else throw new Exception("Password must not be an empty field or null");
+        }
+
+        public User()
+        {
+
+        }
+        
+    }
+}
