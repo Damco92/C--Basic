e41746148e312c62e76fdf5205ae1542b6bae74c
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-03-26 15:19:51 +0100

    Class 8 exe 1 done

---
+++ Class_8/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>()
+            {
+                {"Damjan", "075-123-432" },
+                {"Riste", "072-123-432" },
+                {"Ivan", "070-232-131" },
+                {"Borjan", "071-123-123" },
+                {"Martin", "070-123-123" }
+            };
+
+            Console.WriteLine("Please enter your name");
+            string inputName = Console.ReadLine();
+            string phoneNumber;
+            bool foundName = phoneBook.TryGetValue(inputName, out phoneNumber);
+            if (foundName)
+            {
+                Console.WriteLine($"{inputName}'s phone number is {phoneNumber}");
+            }
+            Console.Read();
+
+        }
+    }
+}
