db15c6065f1c89d70a73a7b74b1004b67caaecf2
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-03-26 16:39:05 +0100

    class 8 exe 2 basic

---
+++ Class_8/ClassLibrary1/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string ID { get; set; } = "0";
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Genre FavoreteMusicType;
+        List<Song> favoriteSongs = new List<Song>();
+
+        public Person(string first,string last,int age,Genre gen)
+        {
+            FirstName = first;
+            LastName = last;
+            Age = age;
+            FavoreteMusicType = gen;
+        }
+
+
+    }
+}
---
+++ Class_8/ClassLibrary1/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public int Length { get; set; }
+        public Genre Genre;
+
+        public Song(string title,int length,Genre gen)
+        {
+            Title = title;
+            Length = length;
+            Genre = gen;
+        }
+    }
+}
